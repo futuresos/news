a0ce13e994c341b7c26f3c7a781aae382b4e9c84
futuresos <42096453+futuresos@users.noreply.github.com>
2018-09-22 09:13:43 +0800

    O

--- JS/pgt.js
+++ JS/pgt.js
@@ -1,2 +1,2 @@
 
-document.writeln("<img src=\'https://www.paogou444.com/paogou.jpg\' width=\'100%\'height=\'100%\' title=\'跑狗图\' /></a>");
+document.writeln("<img src=\'https://www.paogou444.com/paogou.jpg\' width=\'100%\'height=\'100%\'  /></a>");

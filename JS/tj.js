23b9a9c6af5a7aa711a5194e29a27a766e0163a5
futuresos <42096453+futuresos@users.noreply.github.com>
2019-07-19 00:31:02 +0800

    OK

--- JS/tj.js
+++ JS/tj.js
@@ -1,1 +1,1 @@
-﻿document.write ('<script type="text/javascript"  src="https://js.users.51.la/20100995.js"></script>');
+document.write ('<script type="text/javascript"  src="https://js.users.51.la/19986201.js"></script>');
